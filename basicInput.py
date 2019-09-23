b304d4d8ed93d2c62eda04bce6971e3a5b4eaaa6
New <new@OrsonsdamondMBP.lan>
2019-09-23 14:00:06 +0100

    Asking for users age

--- basicInput.py
+++ basicInput.py
@@ -3,3 +3,5 @@ print ("Hello world")
 myName = input("What is your name? ")
 print ("Hello " + myName + " it is nice to meet you")
 
+myAge = input("what is your age? ")
+print("you will be " +  str(int(myAge)+1 ) + " In a years time")
