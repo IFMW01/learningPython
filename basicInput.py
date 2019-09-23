7061ed47b315dae93bcfdbf0135f76ac01666e6f
New <new@OrsonsdamondMBP.lan>
2019-09-23 14:10:05 +0100

    Created a new test branch

--- basicInput.py
+++ basicInput.py
@@ -5,3 +5,5 @@ print ("Hello " + myName + " it is nice to meet you")
 
 myAge = input("what is your age? ")
 print("you will be " +  str(int(myAge)+1 ) + " In a years time")
+
+print ("This is  a new branch test")
